28a64b454ed6e21da5779bb12a4de546fdffb4ed
Đỗ Trọng Khánh <79818873+trong-khanh-1109@users.noreply.github.com>
2021-06-20 17:23:49 +0700

    Create largest_number.py

---
+++ Self Training/DoTrongKhanh_19521676/Coursera Algorithmic Toolbox/Week_3/largest_number.py
@@ -0,0 +1,21 @@
+#Uses python3
+
+import sys
+
+def largest_number(a):
+	#write your code here
+	res = ""
+	newlist = []
+	while len(a):
+		first = a[0]
+		for x in a[1:]:
+			if first*len(x)<x*len(first): first = x
+		a.remove(first)
+		res += first
+	return res
+
+if __name__ == '__main__':
+	input = sys.stdin.read()
+	data = input.split()
+	a = data[1:]
+	print(largest_number(a))
