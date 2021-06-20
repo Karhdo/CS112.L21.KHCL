9f2e446419c05fe28fb76f5a95c567eda3c2e152
Đỗ Trọng Khánh <79818873+trong-khanh-1109@users.noreply.github.com>
2021-06-20 17:23:00 +0700

    Create FractionalKnapsack.py

---
+++ Self Training/DoTrongKhanh_19521676/Coursera Algorithmic Toolbox/Week_3/FractionalKnapsack.py
@@ -0,0 +1,30 @@
+import sys
+
+def get_optimal_value(capacity, weights, values):
+    values = [float(values[i])/float(weights[i]) for i in range(len(values))]
+    aka = sorted(values,reverse = True)
+    filled = 0
+    answer = 0
+    i = 0
+    weights = [weights[values.index(aka[i])] for i in range(len(values))]
+
+    values = aka
+    while filled<capacity and i<len(values):
+        if weights[i]+filled>capacity:
+
+            return answer+ (values[i]*(capacity-filled))
+        else:
+            answer += values[i]*weights[i]
+            filled+=weights[i]
+
+            i+=1
+    return answer
+
+
+if __name__ == "__main__":
+    data = list(map(int, sys.stdin.read().split()))
+    n, capacity = data[0:2]
+    values = data[2:(2 * n + 2):2]
+    weights = data[3:(2 * n + 2):2]
+    opt_value = get_optimal_value(capacity, weights, values)
+    print("{:.10f}".format(opt_value))
