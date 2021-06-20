bdfd920036f4248dfe76a1224eb10bcc3a899ea3
Đỗ Trọng Khánh <79818873+trong-khanh-1109@users.noreply.github.com>
2021-06-20 17:36:18 +0700

    Update CSRanking_Tiệm_đồ_chơi.py

--- Self Training/DoTrongKhanh_19521676/CSRanking_Tiệm_đồ_chơi.py
+++ Self Training/DoTrongKhanh_19521676/CSRanking_Tiệm_đồ_chơi.py
@@ -1,23 +1,24 @@
-def twoSum(nums, target):
-        numIndex = dict()
+def Sum_two(nums, target):
+        Num_index = dict()
         for i in range(len(nums)):
             n = nums[i]
             n2 = target - n
-            if n2 in numIndex:
-                return [numIndex[n2]+1,i+1]
-            numIndex[nums[i]] = i
+            if n2 in Num_index:
+                return [Num_index[n2]+1,i+1]
+            Num_index[nums[i]] = i
         return None
-n=int(input())
-arr=[]
+
+n = int(input())
+lst = []
 for i in range(0,n):
-    m=int(input())
-    n1=int(input())
-    arr=list(map(int, input().strip().split()))[:n1]
-    if len(arr)<2:
+    m = int(input())
+    n1 = int(input())
+    lst = list(map(int, input().strip().split()))[:n1]
+    if len(lst) < 2:
         print(*[0,0])
     else:
-        s=twoSum(arr,m)
-        if s!=None:
+        s = Sum_two(lst,m)
+        if s != None:
             print(*s)
         else:
             print(*[0,0])
